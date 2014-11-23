63469a3fca6b3dbfe3eef94e2fd7e5a137994141
MuchContact <MuchContact@users.noreply.github.com>
2014-11-23 23:46:19 +0800

    NumberFormater测试驱动：新增测试用例，完善小数格式处理实现。

--- src/tax/com/NumberFormater.java
+++ src/tax/com/NumberFormater.java
@@ -1,10 +1,15 @@
 package tax.com;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
 public class NumberFormater {
 
-	public double roundWith2Decimals(double d) {
-		// TODO Auto-generated method stub
-		return .89;
+	public double roundWith2Decimals(double number) {
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(2);
+		nf.setMinimumFractionDigits(2);
+		return new BigDecimal(nf.format(number)).doubleValue();
 	}
 
 }
--- test/tax/com/NumberFormaterTest.java
+++ test/tax/com/NumberFormaterTest.java
@@ -10,4 +10,14 @@ public class NumberFormaterTest {
 		NumberFormater nf = new NumberFormater();
 		Assert.assertEquals(.89, nf.roundWith2Decimals(0.889));
 	}
+	@Test
+	public void round_0_881_With2Decimal_return_0_88(){
+		NumberFormater nf = new NumberFormater();
+		Assert.assertEquals(.88, nf.roundWith2Decimals(0.881));
+	}
+	@Test
+	public void round_1_1_With2Decimal_return_1_10(){
+		NumberFormater nf = new NumberFormater();
+		Assert.assertEquals(1.10, nf.roundWith2Decimals(1.1));
+	}
 }

6b6dc39b0950e80c68010109996f747e6553076f
MuchContact <MuchContact@users.noreply.github.com>
2014-11-24 10:07:30 +0800

    TaxCalculator测试驱动：通过测试生成roundup功能实现，在roundup的实现过程中再次抽取出divisibleBy0_05和roundupToNearestFivePercent函数实现。

--- src/tax/com/TaxCalculator.java
+++ src/tax/com/TaxCalculator.java
@@ -28,9 +28,28 @@ public class TaxCalculator {
 		return priceWithoutTax*getTaxRate() + priceWithoutTax;
 	}
 
-	public double roundupToNearestFivePercent(double d) {
-		// TODO Auto-generated method stub
-		return 0.15;
+	public double roundup(double number) {
+		if(divisibleBy0_05(number)){
+			return number;
+		}
+		return roundupToNearestFivePercent(number);
+	}
+	private boolean divisibleBy0_05(double dividend){
+		boolean numberAfterSecondDecimalDigitEqualsZero=(dividend*100==(int)(dividend*100));
+		boolean numberBeforeThirdDecimalDigitDivisibleBy0_05 = (int)(dividend*100)%5==0;
+		return numberAfterSecondDecimalDigitEqualsZero&&
+				numberBeforeThirdDecimalDigitDivisibleBy0_05;
+	}
+	private double roundupToNearestFivePercent(double number){
+		number += .05;
+		int result=(int)(number*100);
+		int lastDigit=result%10;
+		if(lastDigit>=5){
+			result = result-(lastDigit-5);
+		}else{
+			result = result-(lastDigit-0);
+		}
+		return result/100d;
 	}
 
 }

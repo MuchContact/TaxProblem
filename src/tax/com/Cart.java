c7b7b8f76adbff3ffe73c30a42c9d53beef5f853
MuchContact <MuchContact@users.noreply.github.com>
2014-11-24 23:25:36 +0800

    Cart购物车测试驱动：初步形成购物车的add()和toString()方法。下一步暂转移至Parser解析器测试，即从输入商品中解析关键信息。

--- src/tax/com/Cart.java
+++ src/tax/com/Cart.java
@@ -4,21 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
+	private static final String newLine="\r\n";
+	
+	private List<Item> items;
 	
 	public Cart() {
 	}
 	public Cart add(String source) {
-		List<String> items = new ArrayList<String>();
-		items.add(source);
+		items = new ArrayList<Item>();
+		RegItemParser parser = new RegItemParser(source);
+		Bean bean = parser.parseToBean();
+		items.add(Item.createFromBean(bean));
 		return this;
 	}
 	@Override
 	public String toString() {
-		return "1 book : 12.49\r\n"+
-				"1 music CD: 16.49\r\n"+
-				"1 chocolate bar: 0.85\r\n"+
-				"Sales Taxes: 1.50\r\n"+
-				"Total: 29.83\r\n";
+		StringBuilder sb = new StringBuilder();
+		sb.append(itemsDetail());
+		sb.append(salesTaxesDetail());
+		sb.append(totalPriceDetail());
+		return sb.toString();
 	}
-
+	private String itemsDetail() {
+		StringBuilder sb = new StringBuilder();
+		for(Item item:items){
+			sb.append(item.detail()+newLine);
+		}
+		return sb.toString();
+	}
+	private String salesTaxesDetail() {
+		return String.format("Sales Taxes: %.2f%s", getTotalTax(), newLine);
+	}
+	private String totalPriceDetail() {
+		return String.format("Total: %.2f%s", getTotalPrice(), newLine);
+	}
+	private double getTotalPrice() {
+		double totalFee=0;
+		for(Item item:items){
+			totalFee += item.getShelfPrice();
+		}
+		return totalFee;
+	}
+	private double getTotalTax() {
+		double totalTax=0;
+		for(Item item:items){
+			totalTax += item.getTax();
+		}
+		return totalTax;
+	}
 }
--- test/tax/com/CartTest.java
+++ test/tax/com/CartTest.java
@@ -3,6 +3,7 @@ package tax.com;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class CartTest {
 	@Test
 	public void checkReceiptGivenItemsOfInput1(){
@@ -15,5 +16,15 @@ public class CartTest {
 		Assert.assertTrue(cart.toString().contains("1 chocolate bar: 0.85"));
 		Assert.assertTrue(cart.toString().contains("Sales Taxes: 1.50"));
 		Assert.assertTrue(cart.toString().contains("Total: 29.83"));
+	}
+	@Test
+	public void checkReceiptGivenItemsOfInput2(){
+		Cart cart = new Cart();
+		cart.add("1 imported box of chocolates at 10.00")
+			.add("1 imported bottle of perfume at 47.50");
+		Assert.assertTrue(cart.toString().contains("1 imported box of chocolates: 10.50"));
+		Assert.assertTrue(cart.toString().contains("1 imported bottle of perfume: 54.65"));
+		Assert.assertTrue(cart.toString().contains("Sales Taxes: 7.65"));
+		Assert.assertTrue(cart.toString().contains("Total: 65.15"));
 	}
 }
